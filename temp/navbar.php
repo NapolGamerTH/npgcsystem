0fca7b831e2dc88f3ed8fd5e481048b6507cd6c4
NapolGamer TH <knightnapol15@gmail.com>
2017-07-25 23:20:22 +0700

    Create navbar.php

---
+++ temp/navbar.php
@@ -0,0 +1,21 @@
+        <h1>
+<a href="index.php">NPGCSystem</a></h1>
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">หน้าแรก</a></li>
+                
+                <li><a href="/download.php">ดาวน์โหลด</a></li>
+                
+                <li><a href="/contact.php">ติดต่อ</a></li>
+				
+				<li><a href="/donate.php">โดเนท</a></li>
+                <li>
+                    <a href="#" class="icon fa-angle-down">อื่นๆ</a>
+                    <ul>
+                        <li><a href="#kuyart">...</a></li>
+                        <li><a href="#ihereart">...</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
