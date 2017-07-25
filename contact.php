6e4b6dc002c7e3b52ea1bc2105a1d7492715390a
NapolGamer TH <knightnapol15@gmail.com>
2017-07-25 23:08:44 +0700

    Create contact.php
    added file contact.php

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php require '/temp/head.php'?>
+<title>ติดต่อ | NPGCSystem</title>
+<header id="header">
+<?php require '/temp/navbar.php'?>
+
+<section id="main" class="container">
+
+<section class="box special">
+    <header class="major">
+        <h2>ช่องทางในการติดต่อหรือ
+		<br />
+		รายงานปัญหาตัวรัน NPGCSystem</h2>
+        <p>จะมีช่องทางให้คุณติดต่อกับผมได้ดังนี้</p>
+    </header>
+</section>
+
+<section class="box special features">
+    <div class="features-row">
+        <section>
+            <span class="icon major fa-facebook accent2"></span>
+            <h3>ติดต่อผ่านเฟสบุ๊ค</h3>
+            <p><a href="https://www.facebook.com/NapolGamerTH" class="button alt small">ติดต่อเลย</a></p>
+        </section>
+        <section>
+            <span class="icon major fa-plus accent3"></span>
+            <h3>ช่องทางอื่นๆ</h3>
+            <p><a href="#ไม่มีบริการ" class="button alt small">ไม่มีบริการ</a></p>
+        </section>
+    </div>
+        </section>
+
+<?php require '/temp/footer.php'?>
