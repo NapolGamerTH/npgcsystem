b519442528046f3864ade737fef5e15393f8ee3d
NapolGamer TH <knightnapol15@gmail.com>
2017-07-25 23:43:04 +0700

    Add files via upload

---
+++ error/401.php
@@ -0,0 +1,24 @@
+<?php require '/error/include/header.php'?>
+  <body class="background error-page-wrapper background-color background-image">
+    <center>
+  <div class="content-container shadow">
+    <div class="head-line secondary-text-color">
+      401
+    </div>
+    <div class="subheader primary-text-color">
+      Unauthorized
+    </div>
+    <div class="hr"></div>
+    <div class="context secondary-text-color">
+      <p>
+        ต้องการกลับหน้าแรกให้คลิก "หน้าแรก"
+      </p>
+    </div>
+    <div class="buttons-container">
+      <a class="button" href="/index.php" target="_blank"><span class="fa fa-home"></span> หน้าแรก</a>
+    </div>
+  </div>
+</center>
+<?php require '/error/include/js/error.js'?> 
+  </body>
+</html>
