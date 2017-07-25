d430f7f828c6518e2c235f38cebb8713274d75d2
NapolGamer TH <knightnapol15@gmail.com>
2017-07-25 23:07:21 +0700

    Create download.php
    added file download.php

---
+++ download.php
@@ -0,0 +1,27 @@
+<?php require '/temp/head.php'?>
+<title>ดาวน์โหลด | NPGCSystem</title>
+<header id="header">
+<?php require '/temp/navbar.php'?>
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2>NPGCSystem</h2>
+						<p>ขั้นตอนในการติดตั้งของตัวรันเซิร์ฟเวอร์ NPGCSystem</p>
+					</header>
+					<div class="row">
+						<div class="12u">
+
+							<!-- Text -->
+								<section class="box">
+								<h3>ติดตั้งบน Windows</h3>
+									<h4>ขั้นตอนที่ 1</h4>
+									<h1>แบบไม่สำเร็จรูป</h1>
+									<p>ดาวน์โหลด <a href="NPGCSystem.zip">NPGCSystem</a> แล้วก็แตกไฟล์ซิปออก นำโฟลเดอร์ "src" ออกมา แล้วนำมาวางในแฟ้มเซิร์ฟของคุณ</p>
+									<h4>ขั้นตอนที่ 2</h4>
+									<p>คุณจำเป็นต้องมี start.cmd เพื่อสตาร์ทเซิร์ฟ "<a href="">ดาวน์โหลด start.cmd</a>" และก็จะมีการแนะนำต่างๆเช่น การติดตั้งเซิร์ฟเวอร์ และภาษา</p>
+									<h4>ขั้นตอนที่ 3</h4>
+									<p>เมื่อคุณสตาร์ทและตั้งค่าหมดแล้ว คุณจะเห็นไฟล์ "server.properties" อยู่ในโฟลเดอร์เซิร์ฟของคุณ คุณก็สามารถตั้งค่าได้เซิร์ฟ ชื่อเซิร์ฟ</p>
+									<h4>ขั้นตอนที่ 4</h4>
+									<p>จะเห็นโฟลเดอร์ "plugins" ในโฟลเดอร์เซิร์ฟของคุณ คุณสามารถลงปลั๊กอินแบบ ".phar" และแบบเป็น "โฟลเดอร์" ได้ เช่นเดียวกับตัวรันเซิร์ฟเวอร์ "Tesseract"</p>
+							</section>
+<?php require '/temp/footer.php'?>
